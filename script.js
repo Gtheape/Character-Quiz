5fed20366ea7be35ca0c1279c1cdaff22bc9064d
Gael Luna <75498183+Gtheape@users.noreply.github.com>
2021-06-04 13:10:55 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,17 @@
+let correctheight = "5";
+let correcttraits = "Protective";
+
+
+$("button").click(function() {
+    let height = $(".height").val();
+    let traits = $(".traits").val();
+    console.log({height});
+    console.log({traits});
+    
+    if(height === "Vanilla" && traits === correcttraits){
+console.log("Bear");
+ 
+ 
+
+
+    } });
